d7b8c9a2e38926fd049bbea0555dac8fb6d9cbf0
aashish347 <aashish.kumar@elucidata.io>
2017-07-14 19:23:00 +0530

    add index.ts which exports modules, components and services

---
+++ index.ts
@@ -0,0 +1,7 @@
+
+export { FileValidationLogsModule } from './src/app/filevalidationlogs.module';
+export { FileTabs } from './src/app/filetabscomponent/filetabs.component';
+export { AccordionView } from './src/app/accordionviewcomponent/accordionview.component';
+export { ListView } from './src/app/listviewcomponent/listview.component';
+export { DataExchangeService } from './src/app/services/dataexchange.service';
+export { DataManipulationService } from './src/app/services/datamanipulation.service';
